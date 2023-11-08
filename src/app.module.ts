03d1271d34050db9b9c73f770a1cd477b2831d5c
praveensrinivasan01 <praveensrinivasan01@gmail.com>
2023-11-08 16:28:20 +0530

    api changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,7 +12,7 @@ import { OrderModule } from './modules/order/order.module';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true }),
-  TypeOrmModule.forRoot(typeOrmConfig), UsersModule, AdminModule, ProductModule, LandingpageModule, OrderModule],
+    TypeOrmModule.forRoot(typeOrmConfig), UsersModule, AdminModule, OrderModule],
   controllers: [AppController],
   providers: [AppService],
 })
